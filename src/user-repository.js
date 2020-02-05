d6fbc25491f254373042d9e9bd3d80473dd049b8
John Watterson <john@breakthrewbrew.com>
2020-02-05 15:21:05 -0700

    Completed user repository class obj

---
+++ src/user-repository.js
@@ -0,0 +1,30 @@
+const userData = require('../data/users.js');
+
+class UserRepository {
+  constructor(userData) {
+    this.users = userData;
+  }
+
+  findUser(userId) {
+    return this.users.find(user => user.id === userId);
+  }
+
+  calculateStepGoalAverage() {
+    let dailyStepGoals = this.users.map(user => user.dailyStepGoal);
+    let total = dailyStepGoals.reduce((total, goal) => {
+      total += goal;
+      return total;
+    }, 0);
+    return Math.round(total / dailyStepGoals.length);
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = UserRepository;
+}
+
+//A UserRepository holds onto all of the User objects
+// It should have a parameter to take in user data
+// It should have methods to determine:
+// Given a user’s ID, what is their user data?
+// The average step goal amongst all users
